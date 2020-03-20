18c16a59bfa8bdbe138cc1a76ee91d90c3c075f8
Bart de Jong <61181432+bartdejongbdj@users.noreply.github.com>
2020-03-20 16:35:32 +0100

    update

--- Mike&Bart/Controller functions/MPC_Controller.m
+++ Mike&Bart/Controller functions/MPC_Controller.m
@@ -1,16 +1,16 @@
 function [results_mpc] = MPC_Controller(DTSS1, mpc_sim, x0, simT, Ts, ulim, xlim)
-m = 0.030;  % weight (in kg) with 5 vicon markers (each is about 0.25g)
-g = 9.81;   % gravitational constant
-I = [1.43e-5,   0,          0; % inertial tensor in m^2 kg
-    0,         1.43e-5,    0;
-    0,         0,          2.89e-5];
-L = 0.046; % arm length in m
-
-params.mass = m;
-params.I    = I;
-params.invI = inv(I);
-params.grav = g;
-params.arm_length = L;
+% m = 0.030;  % weight (in kg) with 5 vicon markers (each is about 0.25g)
+% g = 9.81;   % gravitational constant
+% I = [1.43e-5,   0,          0; % inertial tensor in m^2 kg
+%     0,         1.43e-5,    0;
+%     0,         0,          2.89e-5];
+% L = 0.046; % arm length in m
+% 
+% params.mass = m;
+% params.I    = I;
+% params.invI = inv(I);
+% params.grav = g;
+% params.arm_length = L;
 
 A = DTSS1.A;
 B = DTSS1.B;
@@ -38,8 +38,13 @@ for k = 1:N
         objective = objective + norm(Qt*x{k}, 1);
     end
     constraints = [constraints, x{k+1} == A*x{k} + B*u{k}];
-    %     constraints = [constraints, -ulim <= u{k}<= ulim];
-    %     constraints = [constraints, -xlim<=x{k+1}(3)<=xlim];
+        constraints = [constraints, -ulim(1) <= u{k}(1)<= ulim(1)];
+        constraints = [constraints, -ulim(2) <= u{k}(2)<= ulim(2)];
+        constraints = [constraints, -ulim(3) <= u{k}(3)<= ulim(3)];
+        constraints = [constraints, -ulim(4) <= u{k}(4)<= ulim(4)];
+        constraints = [constraints, -0.0001 <= u{k}(5) <= 0.0001];
+        
+%         constraints = [constraints, -xlim<=x{k+1}(3)<=xlim];
 end
 controller = optimizer(constraints, objective,[],x{1},[u{:,1}]);
 
@@ -50,11 +55,12 @@ xn = x_true;
 f = waitbar(0,'1','Name','Simulating MPC controller',...
     'CreateCancelBtn','setappdata(gcbf,''canceling'',1)');
 for i = 1:simT/Ts
-
+i
     U = controller{xn};
     
     %     [~,x_true] = ode45(@(t,xs) pendulumdynamics(xs, U(1), g, l, b_a, b_p, m, M), [0 Ts], x_true);
     %       [~, x_true] = ode45(@(t,s) quadEOM(t, s, U(:,1), params), [0 Ts], x_true);
+%     x_true = A*x_true;
     x_true = A*x_true + B*U(:,1);
     
     %     x_true = x_true(end, :)';
@@ -64,9 +70,8 @@ for i = 1:simT/Ts
     y = C*x_true;
     xn = x_true;
     
-    
     % Save control inputs and state
-    implementedU = [implementedU;U(1)];
+    implementedU = [implementedU;U(:,1)'];
     state(i, :) = x_true';
     
     waitbar(i/(simT/Ts),f,sprintf(num2str(ceil(i/(simT/Ts)*100))))
